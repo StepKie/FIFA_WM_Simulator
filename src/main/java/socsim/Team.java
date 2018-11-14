b3285d9d77a8a2089a759c374c0136ea41152683
Stephan Kieburg <stephan.kieburg@gmail.com>
2018-11-14 18:12:00 +0100

    Meep

--- src/main/java/socsim/Team.java
+++ src/main/java/socsim/Team.java
@@ -18,37 +18,7 @@ public class Team implements Comparable<Team> {
 	}
 	
 	@Override
-	public int hashCode() {
-		return id == null ? 0 : id.hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Team other = (Team) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	@Override
 	public String toString() {
-		return name;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	String getName() {
 		return name;
 	}
 	
