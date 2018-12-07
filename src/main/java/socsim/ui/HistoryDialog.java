7a94b3b399871a13b25981ecf8ad774db6a7bffd
Stephan Kieburg <stephan.kieburg@gmail.com>
2018-12-07 17:04:14 +0100

    Mooooo

--- src/main/java/socsim/ui/HistoryDialog.java
+++ src/main/java/socsim/ui/HistoryDialog.java
@@ -5,7 +5,9 @@ import java.util.Comparator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
@@ -18,6 +20,7 @@ import socsim.Table.Row;
 import socsim.io.Fussball_IO;
 
 public class HistoryDialog extends Dialog {
+	private static final int DEFAULT_COL_WIDTH = 50;
 	Object result;
 	private Table table;
 	Comparator<Row> comp = socsim.Table.WM_2018;
@@ -36,7 +39,7 @@ public class HistoryDialog extends Dialog {
 		shell.setText("History");
 		// Your code goes here (widget creation, set result, etc).
 		createTable(shell);
-		shell.setSize(600, 800);
+		shell.setSize(650, 800);
 		shell.open();
 		Display display = parent.getDisplay();
 		while (!shell.isDisposed()) {
@@ -48,24 +51,30 @@ public class HistoryDialog extends Dialog {
 	
 	private Table createTable(Composite parent) {
 		
+		parent.setLayout(new FillLayout());
+		GridLayout gridLayout = new GridLayout();
+		Composite composite = new Composite(parent, SWT.FULL_SELECTION);
+		composite.setLayout(gridLayout);
+		
 		var tbl = socsim.Table.buildTable(Fussball_IO.HISTORY, comp);
 		tbl.print(System.out);
-		table = new Table(parent, SWT.BORDER | SWT.FULL_SELECTION);
+		table = new Table(composite, SWT.BORDER | SWT.H_SCROLL | SWT.FULL_SELECTION);
 		GridData gd_table = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
 		table.setLayoutData(gd_table);
+		table.setLinesVisible(true);
 		table.setHeaderVisible(true);
 		
-		createCol("Pl.");
+		createCol("Pl.", DEFAULT_COL_WIDTH);
 		TableColumn t_col_team = new TableColumn(table, SWT.FILL);
 		t_col_team.setWidth(180);
 		t_col_team.setText("Team");
-		createCol("Sp.");
-		createCol("gew.");
-		createCol("un.");
-		createCol("verl.");
-		createCol("Tore", socsim.Table.GOALS_SCORED);
-		createCol("Diff.", socsim.Table.GOAL_DIFFERENCE);
-		createCol("Punkte", socsim.Table.POINTS);
+		createCol("Sp.", DEFAULT_COL_WIDTH);
+		createCol("gew.", DEFAULT_COL_WIDTH);
+		createCol("un.", DEFAULT_COL_WIDTH);
+		createCol("verl.", DEFAULT_COL_WIDTH);
+		createCol("Tore", DEFAULT_COL_WIDTH, socsim.Table.GOALS_SCORED);
+		createCol("Diff.", DEFAULT_COL_WIDTH, socsim.Table.GOAL_DIFFERENCE);
+		createCol("Punkte", DEFAULT_COL_WIDTH, socsim.Table.POINTS);
 		
 		int i = 0;
 		for (Row r : tbl.getRows()) {
@@ -87,8 +96,8 @@ public class HistoryDialog extends Dialog {
 		return table;
 	}
 	
-	private TableColumn createCol(String name, Comparator<Row> c) {
-		var t_col = createCol(name);
+	private TableColumn createCol(String name, int width, Comparator<Row> c) {
+		var t_col = createCol(name, width);
 		t_col.setData(c.reversed());
 		t_col.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -121,9 +130,9 @@ public class HistoryDialog extends Dialog {
 		return t_col;
 	}
 	
-	private TableColumn createCol(String name) {
+	private TableColumn createCol(String name, int width) {
 		TableColumn t_col = new TableColumn(table, SWT.NONE);
-		t_col.setWidth(50);
+		t_col.setWidth(width);
 		t_col.setText(name);
 		
 		return t_col;
